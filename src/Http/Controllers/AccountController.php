62daa57f617230ff43d72e2b2b640f13474ed503
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-07-20 21:40:25 -0400

    add statements to accounts

--- src/Http/Controllers/AccountController.php
+++ src/Http/Controllers/AccountController.php
@@ -5,6 +5,8 @@ namespace Insane\Journal\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
 use Insane\Journal\Account;
 use Insane\Journal\Category;
 use Laravel\Jetstream\Jetstream;
@@ -23,7 +25,13 @@ class AccountController
     public function index(Request $request) {
         return Jetstream::inertia()->render($request, config('journal.accounts_inertia_path') . '/Index', [
             "accounts" => Account::orderBy('index')->get(),
-            "categories" => Category::where('depth', 0)->with(['subCategories', 'subcategories.accounts', 'subcategories.accounts.lastTransactionDate'])->get(),
+            "categories" => Category::where('depth', 0)->with([
+                'subCategories',
+                'subcategories.accounts' => function ($query) use ($request) {
+                    $query->where('team_id', '=', $request->user()->current_team_id);
+                } ,
+                'subcategories.accounts.lastTransactionDate'
+            ])->get(),
         ]);
     }
 
@@ -33,12 +41,67 @@ class AccountController
         $postData['team_id'] = $request->user()->current_team_id;
         $account = new Account();
         $account = $account::create($postData);
-        return $response->sendContent($account);
+        if ($request->query('json')) {
+            return $response->sendContent($account);
+        }
+        return Redirect()->back();
     }
 
     public function show(Request $request) {
         return Jetstream::inertia()->render($request, 'Journal/Accounts/Show', [
             "accounts" => Account::orderBy('index')->get(),
+        ]);
+    }
+
+    public function statements(Request $request, string $category = "income") {
+        $categories = [
+            "income" => "incomes",
+            "expense" => "expenses",
+            "tax" => "taxes"
+        ];
+
+        $categoryData = Category::where('display_id',$categories[$category] )->first();
+
+        $accounts = DB::table('categories')
+        ->where('parent_id', $categoryData->id)
+        ->selectRaw('group_concat(accounts.id) as account_ids, group_concat(accounts.name) as account_names')
+        ->joinSub(DB::table('accounts')->where('team_id', $request->user()->current_team_id), 'accounts','category_id', '=', 'categories.id')
+        ->get();
+
+        $balance = DB::table('transaction_lines')
+        ->whereIn('transaction_lines.account_id', explode(',', $accounts[0]->account_ids))
+        ->selectRaw('sum(amount * transaction_lines.type * accounts.type)  as total, transaction_lines.account_id, accounts.id, accounts.name, accounts.display_id')
+        ->join('accounts', 'accounts.id', '=', 'transaction_lines.account_id')
+        ->groupBy('transaction_lines.account_id')
+        ->get()->toArray();
+
+        $categoryAccounts = Category::where([
+                'depth' => 1,
+                "parent_id" => $categoryData->id,
+            ])->with([
+            'accounts' => function ($query) use ($request) {
+                $query->where('team_id', '=', $request->user()->current_team_id);
+            }
+        ])->get()->toArray();
+
+        $categoryAccounts = array_map(function ($subCategory) use($balance) {
+            $total = [];
+            if (isset($subCategory['accounts'])) {
+                foreach ($subCategory['accounts'] as $accountIndex => $account) {
+                    $index = array_search($account['id'], array_column($balance, 'id'));
+                    if ($index !== false ) {
+                        $subCategory['accounts'][$accountIndex]['balance'] = $balance[$index];
+                        $total[] = $balance[$index]->total;
+                    }
+                }
+            }
+            $subCategory['total'] = array_sum($total);
+            return $subCategory;
+        }, $categoryAccounts);
+
+        return Jetstream::inertia()->render($request, config('journal.statements_inertia_path') . '/Index', [
+            "categories" => $categoryAccounts,
+            'categoryType' => $category
         ]);
     }
 }
